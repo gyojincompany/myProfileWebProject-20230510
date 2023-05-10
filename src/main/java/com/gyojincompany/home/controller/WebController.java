f8d8ed525d097bb8892caf5186e51b0ac92c44b1
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-10 15:11:43 +0900

    index 디자인 완료

--- src/main/java/com/gyojincompany/home/controller/WebController.java
+++ src/main/java/com/gyojincompany/home/controller/WebController.java
@@ -1,5 +1,19 @@
 package com.gyojincompany.home.controller;
 
-public class WebController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class WebController {
+	
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/")
+	public String index2() {
+		return "index";
+	}
+	
 }
