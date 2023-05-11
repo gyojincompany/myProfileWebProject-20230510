67cca9630bebcd927b94d7027844dbf110ff17b6
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-11 09:55:13 +0900

    회원 가입 기능 추가

--- src/main/java/com/gyojincompany/home/controller/WebController.java
+++ src/main/java/com/gyojincompany/home/controller/WebController.java
@@ -1,10 +1,20 @@
 package com.gyojincompany.home.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gyojincompany.home.dao.IDao;
+
 @Controller
-public class WebController {
+public class WebController {	
+	
+	@Autowired
+	private SqlSession sqlSession;	
 	
 	@RequestMapping(value = "/index")
 	public String index() {
@@ -40,4 +50,42 @@ public class WebController {
 	public String question() {
 		return "question";
 	}
+	
+	@RequestMapping(value = "/joinOk")
+	public String joinOk(HttpServletRequest request, Model model) {
+		
+		String mid = request.getParameter("mid");
+		String mpw = request.getParameter("mpw");
+		String mname = request.getParameter("mname");
+		String memail = request.getParameter("memail");
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+	
+		int joinCheck = 0;
+		
+		int checkId = dao.checkIdDao(mid);//가입하려는 id 존재여부 체크 1이면 이미 존재
+		
+		if(checkId == 0) {
+			joinCheck = dao.joinDao(mid, mpw, mname, memail);
+			model.addAttribute("checkId", checkId);
+		//joinCheck 값이 1이면 회원가입 성공, 아니면 가입실패
+		} else {
+			model.addAttribute("checkId", checkId);
+		}
+		
+		if(joinCheck == 1) {
+			model.addAttribute("joinFlag", joinCheck);
+			model.addAttribute("memberName", mname);
+			model.addAttribute("memberId", mid);
+		} else {//회원가입실패
+			model.addAttribute("joinFlag", joinCheck);
+		}
+		
+		return "joinOk";
+	}
+	
+	
+	
+	
+	
 }
